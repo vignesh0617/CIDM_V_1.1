0f6d3ff2b6395a5c6092d1719ab3331819c6e75c
vignesh0617 <63336130+vignesh0617@users.noreply.github.com>
2023-10-11 21:47:57 +0530

    Delete test.py

--- test.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-import mysql.connector as sql
-
-connector =  sql.connnect(username = "root", password = "root" , host = "localhost", database = "dmart")
-
-df = pd.read_sql(sql = "select * from dqsteward")
-
-378867
